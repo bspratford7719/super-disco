86a0650bc0c7a356507ef5a00b79fcbcd7f1455e
brittany spratford <britsprat@gmail.com>
2021-12-05 20:42:06 -0700

    local storage edits

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var nineAm = moment().set({"hour":9, "minute":0, "second":0})
+var tenAm = moment().set({"hour":10, "minute":0, "second":0})
+var elevenAm = moment().set({"hour":11, "minute":0, "second":0})
+var twelvePm = moment().set({"hour":12, "minute":0, "second":0})
+var onePm = moment().set({"hour":13, "minute":0, "second":0})
+var twoPm = moment().set({"hour":14, "minute":0, "second":0})
+var threePm = moment().set({"hour":15, "minute":0, "second":0})
+var fourPm = moment().set({"hour":16, "minute":0, "second":0})
+var fivePm = moment().set({"hour":17, "minute":0, "second":0})
+
 
 const currentDate = moment()
 
@@ -26,15 +36,25 @@ function getTense(moment, id) {
     }
 }
 
-var nineAm = moment().set({"hour":9, "minute":0, "second":0})
-var tenAm = moment().set({"hour":10, "minute":0, "second":0})
-var elevenAm = moment().set({"hour":11, "minute":0, "second":0})
-var twelvePm = moment().set({"hour":12, "minute":0, "second":0})
-var onePm = moment().set({"hour":13, "minute":0, "second":0})
-var twoPm = moment().set({"hour":14, "minute":0, "second":0})
-var threePm = moment().set({"hour":15, "minute":0, "second":0})
-var fourPm = moment().set({"hour":16, "minute":0, "second":0})
-var fivePm = moment().set({"hour":17, "minute":0, "second":0})
+function getUserInputFromLocalStorage(textAreaID){
+    var userInput = localStorage.getItem(textAreaID)
+    document.getElementById(textAreaID).value = userInput
+}
+
+function saveBtn(textAreaID){
+    var userInput = document.getElementById(textAreaID).value;
+    localStorage.setItem(textAreaID, userInput);
+}
+
+document.getElementById('save9').addEventListener("click", function () { saveBtn("nineAmElement")})
+document.getElementById('save10').addEventListener("click", function () { saveBtn("tenAmElement")})
+document.getElementById('save11').addEventListener("click", function () { saveBtn("elevenAmElement")})
+document.getElementById('save12').addEventListener("click", function () { saveBtn("twelvePmElement")})
+document.getElementById('save1').addEventListener("click", function () { saveBtn("onePmElement")})
+document.getElementById('save2').addEventListener("click", function () { saveBtn("twoPmElement")})
+document.getElementById('save3').addEventListener("click", function () { saveBtn("threePmElement")})
+document.getElementById('save4').addEventListener("click", function () { saveBtn("fourPmElement")})
+document.getElementById('save5').addEventListener("click", function () { saveBtn("fivePmElement")})
 
 getTense(nineAm, "nineAmElement")
 getTense(tenAm, "tenAmElement")
@@ -45,5 +65,15 @@ getTense(twoPm, "twoPmElement")
 getTense(threePm, "threePmElement")
 getTense(fourPm, "fourPmElement")
 getTense(fivePm, "fivePmElement")
+
+getUserInputFromLocalStorage("nineAmElement")
+getUserInputFromLocalStorage("tenAmElement")
+getUserInputFromLocalStorage("elevenAmElement")
+getUserInputFromLocalStorage("twelveAmElement")
+getUserInputFromLocalStorage("onePmElement")
+getUserInputFromLocalStorage("twoPmElement")
+getUserInputFromLocalStorage("threePmElement")
+getUserInputFromLocalStorage("fourPmElement")
+getUserInputFromLocalStorage("fivePmElement")
 
 currentDay();
